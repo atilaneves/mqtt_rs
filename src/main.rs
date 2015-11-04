7c059462aaf6178cde6339d9f27b9d3bddc48c44
Atila Neves <aalvesne@cisco.com>
2015-11-04 13:47:24 +0100

    First passing UT for message type

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::io::Write;
 use std::net::TcpListener;
 use std::thread;
 
+mod server;
+mod message;
+
 fn main() {
     let listener = TcpListener::bind(("0.0.0.0", 1883)).unwrap();
     for stream in listener.incoming() {
---
+++ src/message.rs
@@ -0,0 +1,25 @@
+#[derive(PartialEq, Debug)]
+enum MqttType {
+    Connect,
+}
+
+fn message_type(bytes: &[u8]) -> MqttType {
+    MqttType::Connect
+}
+
+#[test]
+fn connect_type() {
+    let mqtt_connect = &[
+        0x10u8, 0x2a, // fixed header
+        0x00, 0x06, 'M' as u8, 'Q' as u8, 'I' as u8, 's' as u8, 'd' as u8, 'p' as u8,
+        0x03, // protocol version
+        0xcc, // connection flags 1100111x user, pw, !wr, w(01), w, !c, x
+        0x00, 0x0a, // keepalive of 100
+        0x00, 0x03, 'c' as u8, 'i' as u8, 'd' as u8, // client ID
+        0x00, 0x04, 'w' as u8, 'i' as u8, 'l' as u8, 'l' as u8, // will topic
+        0x00, 0x04, 'w' as u8, 'm' as u8, 's' as u8, 'g' as u8, // will msg
+        0x00, 0x07, 'g' as u8, 'l' as u8, 'i' as u8, 'f' as u8, 't' as u8, 'e' as u8, 'l' as u8, // username
+        0x00, 0x02, 'p' as u8, 'w' as u8, // password
+        ][0..];
+    assert_eq!(message_type(mqtt_connect), MqttType::Connect);
+}
