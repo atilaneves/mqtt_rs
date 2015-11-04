bb04c1b84fad58e5efc7d352b8d4d9ae15848b49
Atila Neves <aalvesne@cisco.com>
2015-11-04 13:19:52 +0100

    Hello world server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::thread;
+
 fn main() {
-    println!("Hello world");
+    let listener = TcpListener::bind(("0.0.0.0", 1883)).unwrap();
+    for stream in listener.incoming() {
+        thread::spawn(|| {
+            let mut stream = stream.unwrap();
+            stream.write(b"Hello world\r\n").unwrap();
+        });
+    }
 }
