709768ed99d90cacdabf3d4211bb457cdbddc6c5
Atila Neves <atila.neves@gmail.com>
2015-11-04 22:57:22 +0100

    Won't compile

--- src/broker.rs
+++ src/broker.rs
@@ -1,19 +1,39 @@
 pub struct Broker<T: Subscriber> {
-    sub: T,
+    subscriptions: Subscriptions<T>,
 }
 
 impl<T: Subscriber> Broker<T> {
     pub fn new() -> Self {
-        Broker{ sub: T::new()}
+        Broker{ subscriptions: Subscriptions::<T>::new()}
     }
 
     pub fn publish(&self, topic: &str, payload: &[u8]) {
+        self.subscriptions.publish(topic, payload);
+    }
+
+    pub fn subscribe(&mut self, subscriber: &mut T, topics: &[&str]) {
+    }
+}
+
+pub struct Subscriptions<T: Subscriber> {
+    subscribers: Vec<T>,
+}
+
+impl<T: Subscriber> Subscriptions<T> {
+    fn new() -> Self {
+        return Subscriptions { subscribers: vec![], }
+    }
+
+    fn publish(&mut self, topic: &str, payload: &[u8]) {
+        for s in &mut self.subscribers {
+            s.new_message(&[0, 1, 3]);
+        }
     }
 }
 
 pub trait Subscriber {
     fn new() -> Self;
-    fn new_msg(&mut self, bytes: &[u8]);
+    fn new_message(&mut self, bytes: &[u8]);
 }
 
 #[cfg(test)]
@@ -30,15 +50,29 @@ impl Subscriber for TestSubscriber {
         return TestSubscriber{msgs: vec![]}
     }
 
-    fn new_msg(&mut self, bytes: &[u8]) {
+    fn new_message(&mut self, bytes: &[u8]) {
         self.msgs.push(bytes.to_vec());
     }
 }
 
 #[test]
 fn no_subscriptions() {
-    let broker = Broker::<TestSubscriber>::new();
+    let mut broker = Broker::<TestSubscriber>::new();
+    let subscriber = TestSubscriber::new();
     broker.publish("topics/foo", &[0, 1, 3]);
-    assert_eq!(broker.sub.msgs.len(), 0);
+    assert_eq!(subscriber.msgs.len(), 0);
 
+    broker.subscribe(&subscriber, &["topics/foo"]);
+    broker.publish("topics/foo", &[0, 1, 3]);
+    broker.publish("topics/bar", &[2, 4, 6]);
+    assert_eq!(subscriber.msgs.len(), 1);
+    assert_eq!(subscriber.msgs[0], &[0, 1, 3]);
+
+    broker.subscribe(&subscriber, &["topics/bar"]);
+    broker.publish("topics/foo", &[1, 3, 5, 7]);
+    broker.publish("topics/bar", &[2, 4]);
+    assert_eq!(subscriber.msgs.len(), 3);
+    assert_eq!(subscriber.msgs[0], &[0, 1, 3]);
+    assert_eq!(subscriber.msgs[1], &[1, 3, 5, 7]);
+    assert_eq!(subscriber.msgs[1], &[2, 4]);
 }
