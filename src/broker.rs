a375f4656388dcbc854b74a8bcee414da4953dec
Atila Neves <atila.neves@gmail.com>
2015-11-07 15:19:10 +0100

    Passing UT for new subscription tree

--- src/broker.rs
+++ src/broker.rs
@@ -86,34 +86,34 @@ pub trait SubscriberT {
 
 struct Subscription<T: SubscriberT> {
     subscriber: Rc<RefCell<T>>,
-    topics: Vec<String>,
+    //topic: String,
 }
 
 impl<T: SubscriberT> Subscription<T> {
-    fn append_topics(&mut self, topics: &[&str]) {
-        let mut new_topics = topics.to_vec().into_iter().map(|t| t.to_string()).collect();
-        self.topics.append(&mut new_topics);
-    }
-
-    fn topics(&self) -> Vec<String> {
-        self.topics.clone()
+    fn new(subscriber: Rc<RefCell<T>>/*, topic: &str*/) -> Self {
+        Subscription { subscriber: subscriber.clone()}//, topic: topic.to_string() }
     }
 }
 
 struct Node<T: SubscriberT> {
-    children: HashMap<String, Box<Node<T>>>,
+    children: HashMap<String, Node<T>>,
     leaves: Vec<Subscription<T>>,
 }
 
 impl<T: SubscriberT> Node<T> {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Node { children: HashMap::new(), leaves: vec![] }
+    }
+
+    fn add_subscription(&mut self, subscription: Subscription<T>) {
+        self.leaves.push(subscription);
     }
 }
 
 pub struct SubscriptionsT<T: SubscriberT> {
     tree: Node<T>,
 }
+
 
 impl<T: SubscriberT> SubscriptionsT<T> {
     pub fn new() -> Self {
@@ -122,24 +122,71 @@ impl<T: SubscriberT> SubscriptionsT<T> {
 
     pub fn subscribe(&mut self, subscriber: Rc<RefCell<T>>, topics: &[&str]) {
         for topic in topics {
-            let topParts : Vec<&str> = topic.split("/").collect();
-            println!("topParts: ");
-            for part in &topParts {
-                print!("{}, ", part)
-            }
-            println!("");
+            let sub_parts : Vec<&str> = topic.split("/").collect();
+            Self::ensure_node_exists(&sub_parts, &mut self.tree);
+            Self::add_subscription_to_node(&mut self.tree, subscriber.clone(), &sub_parts);
+        }
+    }
 
-            self.tree.children.insert("part".to_string(), Box::new(Node::new()));
+    fn ensure_node_exists(sub_parts: &[&str], node: &mut Node<T>) {
+        if sub_parts.len() == 0 {
+            return;
+        }
+
+        let part = &sub_parts[0][..];
+        if !node.children.contains_key(part) {
+            node.children.insert(sub_parts[0].to_string(), Node::new());
+        }
+
+        Self::ensure_node_exists(&sub_parts[1..], node.children.get_mut(part).unwrap());
+    }
+
+    fn add_subscription_to_node(tree: &mut Node<T>, subscriber: Rc<RefCell<T>>, sub_parts: &[&str]) {
+        if sub_parts.len() < 1 {
+            panic!("oops");
+        }
+
+        let part = &sub_parts[0][..];
+        let node = tree.children.get_mut(part).unwrap();
+        let sub_parts = &sub_parts[1..];
+
+        if sub_parts.len() == 0 {
+            node.add_subscription(Subscription::new(subscriber.clone()));
+        } else {
+            Self::add_subscription_to_node(node, subscriber, sub_parts);
         }
     }
 
     pub fn publish(&self, topic: &str, payload: &[u8]) {
-
+        let pub_parts : Vec<&str> = topic.split("/").collect();
+        Self::publish_impl(&self.tree, &pub_parts, &payload);
     }
 
-    // fn add_or_find(&mut self, subscriber: Rc<RefCell<T>>) -> Rc<RefCell<T>> {
-    // }
+    fn publish_impl(tree: &Node<T>, pub_parts: &[&str], payload: &[u8]) {
+        if pub_parts.len() < 1 {
+            panic!("oops");
+        }
 
+        let part = &pub_parts[0][..];
+        let node = tree.children.get(part);
+        let pub_parts = &pub_parts[1..];
+
+        match node {
+            None => {
+                return; //didn't find a subscriber
+            },
+            Some(node) => {
+                if pub_parts.len() == 0 {
+                    for subscription in &node.leaves {
+                        let subscriber = subscription.subscriber.clone();
+                        subscriber.borrow_mut().new_message(payload);
+                    }
+                } else {
+                    Self::publish_impl(node, pub_parts, payload);
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -157,6 +204,7 @@ impl TestSubscriberT {
 #[cfg(test)]
 impl SubscriberT for TestSubscriberT {
     fn new_message(&mut self, bytes: &[u8]) {
+        println!("new message with bytes {:?}", bytes);
         self.msgs.push(bytes.to_vec());
     }
 }
@@ -165,19 +213,13 @@ impl SubscriberT for TestSubscriberT {
 fn test_subt() {
     let mut broker = SubscriptionsT::<TestSubscriberT>::new();
     let sub_ref = Rc::new(RefCell::new(TestSubscriberT::new()));
-    let mut subscriber = sub_ref.clone();
+    let subscriber = sub_ref.clone();
     broker.publish("topics/foo", &[0, 1, 2]);
     assert_eq!(subscriber.borrow().msgs.len(), 0);
 
     broker.subscribe(subscriber.clone(), &["topics/foo"]);
-    broker.publish("topics/foo", &[0, 1, 9]);
-    broker.publish("topics/bar", &[2, 4, 6]);
-    assert_eq!(subscriber.borrow().msgs.len(), 1);
-    assert_eq!(subscriber.borrow().msgs[0], &[0, 1, 9]);
-
-    broker.subscribe(subscriber.clone(), &["topics/foo"]);
-    broker.publish("topics/foo", &[0, 1, 9]);
-    broker.publish("topics/bar", &[2, 4, 6]);
+    broker.publish("topics/foo", &[0, 1, 9]); //should get this
+    broker.publish("topics/bar", &[2, 4, 6]); //shouldn't get this
     assert_eq!(subscriber.borrow().msgs.len(), 1);
     assert_eq!(subscriber.borrow().msgs[0], &[0, 1, 9]);
 
